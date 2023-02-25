4d5b92695c7f849555c4ae16be4aa6a88b97ab57
TagamiMotohiro <105412324+TagamiMotohiro@users.noreply.github.com>
2023-02-25 17:05:36 +0900

    石を置けるようになりました(ルール無視だけど)

--- Test/Test/Bord.h
+++ Test/Test/Bord.h
@@ -2,7 +2,26 @@
 #include "Vector2.h"
 class Bord {
 	public:
-	vector2 pos;
-	const int margin = 70;
+	//”’‚¢Î‚ª’u‚©‚ê‚Ä‚¢‚é‚©•‚¢Î‚ª’u‚©‚ê‚Ä‚¢‚é‚©”»’è‚·‚é—ñ‹“Œ^
+	enum STATE
+	{
+		NONE = 0,
+		WHITE = 1,
+		BLACK = 2
+	};
+	const int margin = 50;
+	STATE stone_State[8][8] ={STATE::NONE};
+	void setState(vector2 index,STATE ChangeState)
+	{
+		stone_State[index.x][index.y] = ChangeState;
+	}
+	Bord();
 	private:
+	
 };
+Bord::Bord() {
+	stone_State[4][3] = { STATE::BLACK };
+	stone_State[3][4] = { STATE::BLACK };
+	stone_State[3][3] = { STATE::WHITE };
+	stone_State[4][4] = { STATE::WHITE };
+}
--- Test/Test/DrawPixel.cpp
+++ Test/Test/DrawPixel.cpp
@@ -5,20 +5,27 @@
 #define WINDOW_HEIGHT 480
 #define BORD_SIZE 560
 bool gameEnd=false;
+bool BeforeMouseState=false;
 Bord bord;
 int white = GetColor(255, 255, 255);
+int black = GetColor(0, 0, 0);
 vector2 MoucePos;
 vector2 pos;
+bool MouseGetDown();
 void MainLoop();
-void DrawBord();
+void DrawBord(Bord::STATE now_State[8][8]);
+void SetStone();
+void ChackMousePoint();
 // プログラムは WinMain から始まります
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
 {
+	ChangeWindowMode(true);
 	if (DxLib_Init() == -1)		// ＤＸライブラリ初期化処理
 	{
 		return -1;			// エラーが起きたら直ちに終了
 	}
 	SetMouseDispFlag(true);
+	SetBackgroundColor(0,255,0,1);
 	while (!gameEnd)
 	{
 		MainLoop();
@@ -27,24 +34,69 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
 
 	return 0;				// ソフトの終了 
 }
-
 void MainLoop()
 {
-	if (CheckHitKeyAll(DX_CHECKINPUT_KEY)) {
+	if (CheckHitKey(KEY_INPUT_ESCAPE)) {
 		gameEnd = true;
 	}
-	DrawBord();
-}
-void DrawBord()
-{
-	//8*8の盤面を描画
-	for (int y=0;y<9;y++) 
+	ClearDrawScreen();
+	if (MouseGetDown())
 	{
-		
-		for (int x = 0; x < 9; x++)
-		{
-			DrawLine(bord.margin * x, 0, bord.margin * x, BORD_SIZE, white, 1);
-			DrawLine(0, bord.margin * y, BORD_SIZE, bord.margin * y, white, 1);
+		if((MoucePos.x>0 && MoucePos.x<bord.margin * 8) &&
+		(MoucePos.y>0 && MoucePos.y<bord.margin * 8)) {
+			vector2 posindex;
+			posindex.x = MoucePos.x / bord.margin;
+			posindex.y = MoucePos.y / bord.margin;
+			bord.setState(posindex,Bord::STATE::BLACK);
 		}
 	}
+	ChackMousePoint();
+	DrawBord(bord.stone_State);
+	ScreenFlip();
+}
+void DrawBord(Bord::STATE now_State[8][8])
+{
+	//8*8の盤面を描画
+	for (int y=0;y<8;y++) 
+	{
+		for (int x = 0; x < 8; x++)
+		{
+			//毎フレーム画を更新しているので線も毎フレーム描画
+			DrawLine(bord.margin * x, 0, bord.margin * x, bord.margin*8, black, 1);
+			DrawLine(0, bord.margin * y, bord.margin*8, bord.margin * y, black, 1);
+			//インデックス内の状態を見て石を描画
+			switch (now_State[x][y])
+			{
+			case Bord::STATE::NONE:
+				break;
+			case Bord::STATE::BLACK:
+				DrawCircle(bord.margin / 2 + (bord.margin * x), bord.margin / 2 + (bord.margin * y), bord.margin / 2, black);
+				break;
+			case Bord::STATE::WHITE:
+				DrawCircle(bord.margin / 2 + (bord.margin * x), bord.margin / 2 + (bord.margin * y), bord.margin / 2, white);
+				break;
+			}
+		}
+		
+	}
+	//蓋の線は別途描画
+	DrawLine(bord.margin * 8, 0, bord.margin * 8, bord.margin * 8, black, 1);
+	DrawLine(0, bord.margin *8, bord.margin * 8, bord.margin * 8, black, 1);
+	//石を置くときの式
+	//マスごとの間隔/2+(マスごとの間隔*インデックス)
+}
+void ChackMousePoint()
+{
+	//マウス座標チェック(デバッグ用)
+	GetMousePoint(&MoucePos.x, &MoucePos.y);
+	DrawFormatString(0, 0, white, "Mouse_X=%d::Mouse_Y=%d",MoucePos.x,MoucePos.y);
+}
+bool MouseGetDown()
+{
+	bool nowState = false;
+	bool Get = (GetMouseInput() & MOUSE_INPUT_LEFT) != 0;
+	bool Changed = Get ^ (BeforeMouseState&Get);
+	nowState = Changed ;
+	BeforeMouseState = Get;
+	return Changed;
 }
